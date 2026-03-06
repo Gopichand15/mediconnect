1dac31703c48c1530c7717dd4f318f3f06cc6979
Gopichand15 <gopibodige15@gmail.com>
2026-03-06 16:53:47 +0530

    changed hospital model

--- backend/Mediconnect_backend/hospital/models.py
+++ backend/Mediconnect_backend/hospital/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Hospital(models.Model):
     hospital_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True, null=True)
+    password = models.CharField(max_length=255, null=True)
+    phone = models.CharField(max_length=15, null=True)
+
+    
