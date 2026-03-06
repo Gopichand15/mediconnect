cae17eb8623cd237be3e53aaddcb5f3f3b20966a
Varshitha Tanmai <varshitha@example.com>
2026-03-06 16:28:27 +0530

    created hospital model

--- backend/Mediconnect_backend/hospital/models.py
+++ backend/Mediconnect_backend/hospital/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Hospital(models.Model):
+    hospital_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
