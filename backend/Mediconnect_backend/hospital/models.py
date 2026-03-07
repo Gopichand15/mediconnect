daedd6132682052afc6712d6914db6098ef42b0e
Gopichand15 <gopibodige15@gmail.com>
2026-03-07 11:07:01 +0530

    added hospital model

--- backend/Mediconnect_backend/hospital/models.py
+++ backend/Mediconnect_backend/hospital/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Hospital(models.Model):
+   
     hospital_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    email = models.EmailField(unique=True, null=True)
-    password = models.CharField(max_length=255, null=True)
-    phone = models.CharField(max_length=15, null=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     
